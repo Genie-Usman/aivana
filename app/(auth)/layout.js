c3fba798ad3268d70f4ae61b3b5f40ca6514dcef
Muhammad Usman <155563664+Genie-Usman@users.noreply.github.com.>
2025-03-20 01:56:53 +0500

    Setting some things up

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -2,11 +2,10 @@ import React from 'react'
 
 const Layout = ({ children }) => {
     return (
-        <main className='flex-center min-h-screen w-full bg-[#F4F7FE]'>
+        <main className='flex justify-center items-center min-h-screen w-full bg-[#F4F7FE]'>
             {children}
         </main>
     )
 }
 
 export default Layout
-{ children }
--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,18 @@
 import { IBM_Plex_Sans, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider, SignInButton, SignUpButton, SignedIn, SignedOut, UserButton, } from '@clerk/nextjs'
 
 const IBMPlex = IBM_Plex_Sans({
-  variable: "--font-ibm-plex-sans",
+  variable: "--font-ibm-plex",
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata = {
   title: "Aivana",
@@ -20,10 +21,21 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={cn("antialiased", IBMPlex.variable, geistMono.variable)}>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider appearance={{ variables: { colorPrimary: "#624cf5" } }}>
+      <html lang="en">
+        <body className={cn("antialiased bg-red ", IBMPlex.variable)}>
+          <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
